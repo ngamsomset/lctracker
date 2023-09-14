cf98bd6632608bea800752fe41ee10b775235fc0
ngamsomset <setthasiri.ngm@gmail.com>
2023-09-14 19:38:11 +1000

    LastStoneWeight

---
+++ src/main/java/heap/LastStoneWeight.java
@@ -0,0 +1,45 @@
+package heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Given an arrays of stones where all of the number in an array represent the weight of each
+ * stone. On each turn, we choose two heaviest stones and smash them together with
+ * conditions of
+ * 1. if x == y, both stones are destroyed
+ * 2. if x != y, x is destroyed and the stone of y has new weight of y - x.
+ *
+ * Intuition
+ * the key word here is "heaviest" stones. which means we will use maxHeap to solve this problem.
+ *
+ * Process.
+ * 1. Create a maxHeap from a given array.
+ * 2. loop through heap and get top 2 stones and compare them.
+ * 3. return the last stone.
+ */
+
+public class LastStoneWeight {
+    public static void main(String[] args) {
+        int[] stones = new int[]{2,7,4,1,8,1};
+        System.out.println(maxHeaplastStoneWeight(stones));
+    }
+
+    public static int maxHeaplastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int stone:stones) {
+            heap.add(stone);
+        }
+
+        while (heap.size() > 1) {
+            int stone1 = heap.remove();
+            int stone2 = heap.remove();
+
+            if(stone1 != stone2) {
+                heap.add(stone1 - stone2);
+            }
+        }
+
+        return heap.isEmpty() ? 0 : heap.remove();
+    }
+}
